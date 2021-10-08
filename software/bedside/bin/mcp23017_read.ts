0014b5763897615375d35158ea139bf1917a50b2
Richard Miller-Smith <richard@darwin-innovation.com>
2021-10-08 16:28:07 +0100

    update test

--- software/bedside/bin/mcp23017_read.ts
+++ software/bedside/bin/mcp23017_read.ts
@@ -1,42 +1,23 @@
-import '../server/common/env';
-import l from '../server/logger';
+import "../server/common/env";
+import l from "../server/logger";
+import { parse } from "json5";
+import { readFileSync } from "fs";
 
-import { MCP23017, MCP23017PortId } from '../lib/MCP23017';
+import { MCP23017, MCP23017PortId } from "../lib/MCP23017";
 MCP23017.Init();
-import { WbI2cBus } from '../lib/WbI2cBus';
+import { WbI2cBus } from "../lib/WbI2cBus";
 
 const i2cBus = new WbI2cBus(l, {
   busId: 1,
-  devices: [
-    {
-      address: 0x20,
-      type: 'mcp',
-      name: 'okin',
-      details: {
-        A: {
-          int_gpio: 25,
-          read_bits: 0x7f,
-          poll_period: 0,
-          default: 0x7f
-        }
-      },
-    },
-    // {
-    //   address: 0x24,
-    //   type: 'mcp',
-    //   name: 'okin_control',
-    // },
-
-  ],
+  devices: parse(readFileSync("./cfg/i2c.json5").toString()),
 });
 
-const mcp: MCP23017 = i2cBus.FindByName('okin');
+const mcp: MCP23017 = i2cBus.FindByName("buttons");
 
 const A = mcp.getPort(MCP23017PortId.A);
-A.on('change', (diff: number, curValue: number) => {
+A.on("change", (diff: number, curValue: number) => {
   l.info("Change %s %s", diff.toString(16), curValue.toString(16));
 });
-
 
 // head up 1 down 8
 // feet up 2 down 4
