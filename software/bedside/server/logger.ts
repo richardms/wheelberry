50c53c470ba25f6f759cc08f64fdd59690c17c8c
Richard Miller-Smith <richard@darwin-innovation.com>
2021-10-08 16:27:00 +0100

    abstract logger 2

--- software/bedside/server/logger.ts
+++ software/bedside/server/logger.ts
@@ -1,8 +1,9 @@
-import pino from 'pino';
+import pino from "pino";
+import { Logger } from "../lib/Logger";
 
 const l = pino({
   name: process.env.APP_ID,
   level: process.env.LOG_LEVEL,
-});
+}) as Logger;
 
 export default l;
